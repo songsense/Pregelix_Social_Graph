2ca6a349eca28d49b92f4c73616f9c6ef6b2e8b6
moophis <moophis@gmail.com>
2014-04-24 22:04:07 -0700

    begin to deal with database interface

---
+++ src/main/java/edu/uci/ics/biggraph/servlet/Commander.java
@@ -0,0 +1,16 @@
+package edu.uci.ics.biggraph.servlet;
+
+/**
+ * Send AQL (AstrixDB Query Language) to interface with output data 
+ * and database.
+ */
+public class Commander {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
---
+++ src/main/java/edu/uci/ics/biggraph/servlet/URLGenerator.java
@@ -0,0 +1,16 @@
+package edu.uci.ics.biggraph.servlet;
+
+import javax.ws.rs.core.UriBuilder;
+
+/**
+ * Translate AQL into valid URL. 
+ */
+public class URLGenerator {
+
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
