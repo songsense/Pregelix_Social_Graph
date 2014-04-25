7d77ee393bac421d3499b93e6cc03a63c36cbce2
moophis <moophis@gmail.com>
2014-04-24 23:40:46 -0700

    API to asterixDB: half done

--- src/main/java/edu/uci/ics/biggraph/servlet/URLGenerator.java
+++ src/main/java/edu/uci/ics/biggraph/servlet/URLGenerator.java
@@ -1,16 +1,26 @@
 package edu.uci.ics.biggraph.servlet;
 
-import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Translate AQL into valid URL. 
  */
 public class URLGenerator {
-
+    static String generate(String host, int port, RestAPI type, String cmd) {
+        URI uri = null;
+        
+        try {
+            uri = new URI("http", null, host, port, type.getQuery(), type.getFrag() + cmd, null);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return uri != null ? uri.toASCIIString() : null;
+    }
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        System.out.println(generate("localhost", 19001, RestAPI.DDL, ""));
     }
 
 }
