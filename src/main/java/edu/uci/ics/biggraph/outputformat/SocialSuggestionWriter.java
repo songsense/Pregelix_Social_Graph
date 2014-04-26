282edc29d4cc27d9ebb9b655a21bd53472ac01df
moophis <moophis@gmail.com>
2014-04-26 16:52:36 -0700

    Add an example for how to write computing results into AsterixDB

--- src/main/java/edu/uci/ics/biggraph/outputformat/SocialSuggestionWriter.java
+++ src/main/java/edu/uci/ics/biggraph/outputformat/SocialSuggestionWriter.java
@@ -2,6 +2,7 @@ package edu.uci.ics.biggraph.outputformat;
 
 import java.io.IOException;
 
+import edu.uci.ics.biggraph.servlet.URLGenerator;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.RecordWriter;
 
@@ -10,6 +11,8 @@ import edu.uci.ics.biggraph.io.VLongArrayListWritable;
 import edu.uci.ics.biggraph.io.VLongWritable;
 import edu.uci.ics.pregelix.api.graph.Vertex;
 import edu.uci.ics.pregelix.api.io.text.TextVertexOutputFormat.TextVertexWriter;
+
+import edu.uci.ics.biggraph.servlet.*;
 
 public class SocialSuggestionWriter extends 
 TextVertexWriter<VLongWritable, VLongArrayListWritable, IntWritable>{
@@ -23,7 +26,20 @@ TextVertexWriter<VLongWritable, VLongArrayListWritable, IntWritable>{
             Vertex<VLongWritable, VLongArrayListWritable, IntWritable, ?> vertex)
             throws IOException, InterruptedException {
         VLongArrayListWritable val = vertex.getVertexValue();
-        getRecordWriter().write(new Text(vertex.getVertexId().toString()), 
-                new Text(val.toString()));
+        String nodeID = vertex.getVertexId().toString();
+        String nodeVal = val.toString();
+
+        getRecordWriter().write(new Text(nodeID), new Text(nodeVal));
+
+        // Assemble vertex payload as part of AQL UPDATE command.
+        String[] items = new String[2];
+        items[0] = "\"node_id\":" + nodeID;
+        items[1] = "\"suggested_friends\":" + nodeVal; // XXX: NOT SURE!
+
+        String aql = URLGenerator.update("Tasks", "TaskThreeType", items);
+        aql = URLGenerator.cmdParser(aql);
+        String url = URLGenerator.generate("localhost", 19002, RestAPI.UPDATE, aql);
+        Commander.sendGet(url); // no payload to get
     }
+
 }
