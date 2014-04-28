79cf79c88d3a6eba7411ad6be674b089b66c3e21
moophis <moophis@gmail.com>
2014-04-28 13:28:19 -0700

    finish TaskTwoTypeAccessor

---
+++ src/main/java/edu/uci/ics/biggraph/servlet/TaskTwoTypeAccessor.java
@@ -0,0 +1,92 @@
+package edu.uci.ics.biggraph.servlet;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import java.io.IOException;
+
+/**
+ * Created by liqiangw on 4/28/14.
+ */
+public class TaskTwoTypeAccessor extends DataTypeAccessor {
+    private static TaskTwoTypeAccessor ourInstance = new TaskTwoTypeAccessor();
+
+    public static TaskTwoTypeAccessor getInstance() {
+        return ourInstance;
+    }
+
+    /* Fields specification */
+    /** node_id: int32 (primary key) */
+    private int node_id;
+    /** community_id: int32 */
+    private int community_id;
+
+    private TaskTwoTypeAccessor() {
+    }
+
+    public void setVertex(int node_id, int community_id) {
+        assert node_id >= 0;
+
+        this.node_id = node_id;
+        this.community_id = community_id;
+    }
+
+    /**
+     * Load the data entry from database.
+     */
+    @Override
+    public void loadEntry() throws IOException {
+
+    }
+
+    /**
+     * Write back the entry to database.
+     *
+     * Note that this method should be called after calling setVertex()
+     * otherwise unexpected things will happen.
+     */
+    @Override
+    public void storeEntry() throws IOException {
+        removeEntry();
+
+        String url = makeURL();
+        Commander.sendGet(url);
+    }
+
+    private String assembleFields() {
+        JsonObjectBuilder model = Json.createObjectBuilder()
+                .add("node_id", node_id)
+                .add("community_id", community_id);
+
+        return model.build().toString();
+    }
+
+    private String makeURL() {
+        String cmds = assembleFields();
+        String aql = URLGenerator.update("Tasks", "TaskTwo", cmds);
+        aql = URLGenerator.cmdParser(aql);
+        String url = URLGenerator.generate("localhost", 19002, RestAPI.UPDATE, aql);
+
+        return url;
+    }
+
+    private void removeEntry() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("use dataverse Tasks;");
+        sb.append("delete $t from dataset TaskTwo where $t.node_id = " +
+                node_id + ";");
+
+        String cmd = sb.toString();
+        cmd = URLGenerator.cmdParser(cmd);
+        cmd = URLGenerator.generate("localhost", 19002, RestAPI.UPDATE, cmd);
+        Commander.sendGet(cmd);
+    }
+
+    public static void main(String[] args) {
+        TaskTwoTypeAccessor one = getInstance();
+
+        one.setVertex(1, 2);
+        System.out.println(one.assembleFields());
+        System.out.println(one.makeURL());
+    }
+}
